0a3b06a38c52ff3513cdb92a644ceeb04d7fa62c
Dadamuhames <msd2007msd02@gmail.com>
2026-05-24 00:43:34 +0500

    fix: migration command importable

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"github.com/Dadamuhames/go-way/migrate"
 	"github.com/spf13/cobra"
 )
 
 func main() {
 	var rootCmd = &cobra.Command{Use: "app"}
 
-	rootCmd.AddCommand(MigrationCommand())
+	rootCmd.AddCommand(migrate.MigrationCommand())
 
 	rootCmd.Execute()
 }
---
+++ migrate/command.go
@@ -0,0 +1,31 @@
+package migrate
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Dadamuhames/go-way/internal/database"
+	"github.com/spf13/cobra"
+)
+
+func MigrationCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "migrate",
+		Short: "Applying migrations",
+		Run: func(cmd *cobra.Command, args []string) {
+			dbServer := database.New()
+
+			db := dbServer.GetInstance()
+
+			defer db.Close()
+
+			err := Migrate(db)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println("Migrations applied!")
+		},
+	}
+}
